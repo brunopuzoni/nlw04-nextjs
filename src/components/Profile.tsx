868772f408a6aad0b7325ab0c3bd5c85df3c1e1d
Bruno Puzoni <bpuzoni@gmail.com>
2021-03-03 22:39:00 -0300

    fixed indentation

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,15 +4,15 @@ import styles from "../styles/components/Profile.module.css"
 
 export function Profile() {
   const { level } = useContext(ChallengesContext);
-  
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/brunopuzoni.png" alt="Bruno Franco Puzoni" />
       <div>
         <strong>Bruno Puzoni</strong>
         <p>
-          <img src="icons/level.svg" alt="Level"/>
-          Level { level }
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
         </p>
       </div>
     </div>
